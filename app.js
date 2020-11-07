fc860f43a9aeb3d2a6391d5ffb196acdc62599e9
JongWoo <jongwoo328@gmail.com>
2020-11-08 01:47:57 +0900

    express tutorial

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+	res.send('<h1>Hello World</h1>');
+});
+
+app.get('/dynamic', (req, res) => {
+	let list = '';
+	for (let i = 0; i < 5; i++) {
+		list += '<li>test</li>';
+	}
+	let date = Date();
+	let output = `
+	<html>
+		<head>
+			<title>TEST</title>
+		</head>
+		<body>
+			<h1>Hello</h1>
+			${date}
+			<ul>
+				${list}
+			</ul>
+		</body>
+	</html>`;
+	res.send(output);
+});
+
+app.get('/login', (req, res) => {
+	res.send('login please');
+});
+
+app.get('/cat', (req, res) => {
+	res.send('<img src="/cat.jpg">');
+});
+
+app.listen(port, () => {
+	console.log(`Connected ${port} port!`);
+});
